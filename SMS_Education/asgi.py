8e101f2cd1a06e6df9d939250b418fcf0331ba54
Parv Gupta <parvg555@gmail.com>
2021-05-07 01:32:25 +0530

    initial commit

---
+++ SMS_Education/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SMS_Education project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SMS_Education.settings')
+
+application = get_asgi_application()
