8e9ffe17fd2b7895787b902309d644c6b076235a
Parv Gupta <parvg555@gmail.com>
2021-05-07 02:54:23 +0530

    added login/signup

--- SMS_Education/urls.py
+++ SMS_Education/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('login.urls')),
+    path('',include('teacher.urls')),
 ]
 
 if settings.DEBUG:
