8118a2b87d31a214a6e48ad652ac873c3fed6fa4
Parv Gupta <parvg555@gmail.com>
2021-05-10 02:22:54 +0530

    added timer to recorder

--- static/js/recorder.js
+++ static/js/recorder.js
@@ -5,6 +5,56 @@ var stopButton = document.getElementById('stop');
 var playButton = document.getElementById('play');
 var saveButton = document.getElementById('save');
 var audio = document.getElementById('js-audio');
+const timer = document.getElementById('stopwatch');
+
+var hr = 0;
+var min = 0;
+var sec = 0;
+var stoptime = true;
+
+function startTimer(){
+    console.log("this was called");
+    if(stoptime==true){
+        stoptime = false;
+        timerCycle();
+    }
+}
+
+function stopTimer(){
+    if(stoptime == false){
+        stoptime = true;
+    }
+}
+
+function timerCycle() {
+    if (stoptime == false) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+    hr = parseInt(hr);
+    sec = sec + 1;
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+    }
+    if (min == 60) {
+      hr = hr + 1;
+      min = 0;
+      sec = 0;
+    }
+    if (sec < 10 || sec == 0) {
+      sec = '0' + sec;
+    }
+    if (min < 10 || min == 0) {
+      min = '0' + min;
+    }
+    if (hr < 10 || hr == 0) {
+      hr = '0' + hr;
+    }
+    timer.innerHTML = hr + ':' + min + ':' + sec;
+    setTimeout("timerCycle()", 1000);
+    
+  }
+}
 
 promise.then(function(stream){
     var recorder = new MediaRecorder(stream);
@@ -15,26 +65,34 @@ promise.then(function(stream){
     }
 
     recordButton.addEventListener("click",function(){
-        console.log("record button pressed")
-        recordButton.disable = true;
-        stopButton.disable = false;
-        playButton.disable = true;
+        console.log("record button pressed");
+        // recordButton.setAttribute("disabled");
+        // stopButton.removeAttribute("disabled");
+        // playButton.setAttribute("disabled");
+        recordButton.disabled = true;
+        stopButton.disabled = false;
+        playButton.disabled = true;
+        startTimer();
         recorder.start();
     });
 
     stopButton.addEventListener("click",function(){
-        console.log("stop button pressed")
-        stopButton.disable = true;
-        recordButton.disable = false;
-        playButton.disable = false;
+        console.log("stop button pressed");
+        // stopButton.setAttribute("disabled");
+        // recordButton.removeAttribute("disabled");
+        // playButton.removeAttribute("disabled");
+        stopButton.disabled = true;
+        recordButton.disabled = false;
+        playButton.disabled = false;
         recorder.stop();
+        stopTimer();
     });
 
     playButton.addEventListener("click",function(){
         console.log(this.chunks);
-        recordButton.disable = true;
-        stopButton.disable = true;
-        playButton.disable = true;
+        recordButton.disabled = false;
+        stopButton.disabled = false;
+        playButton.disabled = false;
         var blob = new Blob(chunks);
         const audio = new Audio(URL.createObjectURL(blob));
         audio.play();
